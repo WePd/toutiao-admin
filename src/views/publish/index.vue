a1500a9b3e31d1dc18de1155acd9898d00189772
wepd <weped_x@163.com>
2021-06-24 17:03:26 +0800

    article update finished

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -7,12 +7,12 @@
 							<el-breadcrumb-item>{{ $route.query.id ? '修改文章' : '发布文章'}}</el-breadcrumb-item>
     				</el-breadcrumb>
         </div>
-				<el-form ref="form" :model="article" label-width="40px">
+				<el-form  :model="article" label-width="60px" :rules="formRules" ref="publish-form">
 					<el-form-item label="标题" prop="title">
 						<el-input v-model="article.title" ></el-input>
 					</el-form-item>
 						<el-form-item label="内容" prop="content">
-						<el-tiptap lang="zh" v-model="article.content" :extensions="extensions"  height="400"  placeholder="请输入文章内容 …"></el-tiptap>
+						<el-tiptap lang="zh" v-model="article.content" :extensions="extensions"  height="350"  placeholder="请输入文章内容 …"></el-tiptap>
 					</el-form-item>
 						<el-form-item label="封面">
 							<el-radio-group v-model="article.cover.type">
@@ -22,7 +22,7 @@
 								<el-radio :label="-1">自动</el-radio>
 							</el-radio-group>
 					</el-form-item>
-					<el-form-item label="频道">
+					<el-form-item label="频道" prop="channel_id">
 						<el-select v-model="article.channel_id" placeholder="请选择频道">
 							<el-option :label="channel.name" :value="channel.id" v-for="(channel, index) in channels" :key="index"></el-option>
 						</el-select>
@@ -96,6 +96,30 @@ import { uploadImage } from '@/api/images'
 					new OrderedList(),
 					new Fullscreen(),
       ],
+				formRules: {
+					title: [
+						{ required: true, message: '请输入文章标题', trigger: 'blur' },
+						{ min: 3, max: 30, message: '长度在 3 到 30个字符', trigger: 'blur' }
+					],
+					content: [
+							// { required: true, message: '请输入文章内容', trigger: 'blur' },
+						{	
+							validator(rule, value, callback) {
+								if(value === '<p></p>') {
+									// 验证不通过
+									callback(new Error('请再次输入文章内容'));
+								}else {
+									// 验证通过
+									callback()
+								}
+							} 
+						},
+						{ required: true, message: '请输入文章内容', trigger: 'blur' },	
+				],
+					channel_id: [
+						{ required: true, message: '请选择相应的频道', trigger: 'blur' },	
+					]
+			}
   }
      },
    components: {
@@ -111,11 +135,17 @@ import { uploadImage } from '@/api/images'
 	 },
 	 methods: {
 		 onPublish (draft = false) {
-			 //加一个判断，如果是修改文章，则执行修改操作，否则执行添加操作
-			 if(this.$route.query.id){
+			 //提交前的校验
+			 this.$refs['publish-form'].validate(valid => {
+				 //验证失败 停止提交
+				 if(!valid){
+					 return 
+				 }
+				 //加一个判断，如果是修改文章，则执行修改操作，否则执行添加操作
+				 if(this.$route.query.id){
 				 //更新文章操作
 				 updateArticles(this.$route.query.id, this.article, draft).then( res => {
-					 console.log(res.data)
+					//  console.log(res.data)
 					 this.$message({
 						 message: `${ draft ? '存入草稿' : '发布'}成功`,
 						 type: 'success'
@@ -132,7 +162,11 @@ import { uploadImage } from '@/api/images'
 				//发布成功后跳转到内容管理界面
 				this.$router.push('/article')	
 			 }
-      },
+				 
+			 })
+
+			 
+    },
 			loadChannels () {
 				getArticlesChannels().then( res => {
 					this.channels = res.data.data.channels
