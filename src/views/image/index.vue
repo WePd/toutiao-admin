bcd7b8f157454be648b789275ce8a6ac5c4f0926
wepd <weped_x@163.com>
2021-07-09 10:06:34 +0800

    upload images

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -17,7 +17,7 @@
 							<el-radio-button :label="false" @click.native="loadImages(false)">全部</el-radio-button>
 							<el-radio-button :label="true" @click.native="loadImages(true)">收藏</el-radio-button>
     				</el-radio-group>
-						<el-button size="mini" type="success" @click="" >上传素材</el-button>
+						<el-button size="mini" type="success" @click="dialogUploadVisible = true " >上传素材</el-button>
   				</div>
 					<!-- 素材列表 -->
 							<el-row :gutter="10">
@@ -30,12 +30,21 @@
 								</el-col>
 							</el-row>
     		</el-card>
-				<el-dialog title="收货地址" :visible.sync="dialogUploadVisible">
-					<el-table :data="gridData">
-						<el-table-column property="date" label="日期" width="150"></el-table-column>
-						<el-table-column property="name" label="姓名" width="200"></el-table-column>
-						<el-table-column property="address" label="地址"></el-table-column>
-					</el-table>
+				<!-- 点击上传素材展示弹窗 -->
+				<el-dialog title="上传素材" :visible.sync="dialogUploadVisible" :append-to-body='true'>
+							<el-upload
+								class="upload-demo"
+								drag
+								action="http://api-toutiao-web.itheima.net/mp/v1_0/user/images"
+								:headers="upLoadHeaders"
+								name = 'image'
+								:on-success="onUploadSuccess"
+								:show-file-list="false"
+								multiple>
+								<i class="el-icon-upload"></i>
+								<div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+								<div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+							</el-upload>
 				</el-dialog>
     </div>
 </template>
@@ -45,10 +54,14 @@ import { getImages } from '@/api/images'
 export default {
 	name: 'ImageIndex',
 	data(){
+		const user = JSON.parse(window.localStorage.getItem('user'))
 		return {
 				collect: false,
 				images: [],
-				dialogUploadVisible: false
+				dialogUploadVisible: false,
+				upLoadHeaders:{
+					Authorization: `Bearer ${ user.data.token }`
+				}
 	    }
  	},
 	methods: {
@@ -58,7 +71,14 @@ export default {
 				}).then( res => {
 					this.images = res.data.data.results
 			})
-		 }
+		 },
+		 //上传成功后做的操作
+		  onUploadSuccess(){
+			//  首先关闭弹窗
+				this.dialogUploadVisible = false
+			//  更新素材列表
+				this.loadImages(false)
+		  }
 	},
 	created(){
 		 this.loadImages(false)
